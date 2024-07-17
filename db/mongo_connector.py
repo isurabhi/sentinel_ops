4c8a61b72780b58a1483f35a380b36e5c8ddb05f
Surabhi Rajan <surabhirajan@hotmail.com>
2024-07-17 23:36:56 +0530

    mongo con

--- db/mongo_connector.py
+++ db/mongo_connector.py
@@ -1,12 +1,15 @@
 # db/mongo_connector.py
+import os
 import dns.resolver
 from pymongo import MongoClient
 from config.settings import MONGO_URI, MONGO_DB_NAME
 
 class MongoConnector:
     def __init__(self):
-        dns.resolver.default_resolver = dns.resolver.Resolver(configure=False) 
-        dns.resolver.default_resolver.nameservers = ['20.236.44.162', '20.236.44.162']
+        print(os.environ)
+        if os.getenv('DEBUG_MODE') == 'on':
+            dns.resolver.default_resolver = dns.resolver.Resolver(configure=False) 
+            dns.resolver.default_resolver.nameservers = ['20.236.44.162', '20.236.44.162']
         self.client = MongoClient(MONGO_URI)
         self.db = self.client[MONGO_DB_NAME]
 
