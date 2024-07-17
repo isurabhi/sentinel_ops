f0e80c3fed8523118aba69575c59fc70af5d117c
Surabhi Rajan <surabhirajan@hotmail.com>
2024-07-17 11:42:32 +0530

    v 1.2

--- config/settings.py
+++ config/settings.py
@@ -1,5 +1,6 @@
 # MongoDB configuration
-MONGO_URI = "mongodb://localhost:27017"
+#MONGO_URI = "mongodb://localhost:27017"
+MONGO_URI = "mongodb+srv://isurabhi:mIR8ps4bDv0u7o@aisentinelops.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000"
 MONGO_DB_NAME = "sentinel_ops_db"
 MONGO_COLLECTION_NAME = "ms_teams_freezes"
 MONGO_COLLECTION_SYSTEM_CRASHES = "device_crashes"
